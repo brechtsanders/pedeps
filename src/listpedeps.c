ba02ba0552094635f34479423dcaf62db8757c4b
Brecht Sanders <brecht@sanders.org>
2024-09-10 20:52:39 +0200

    -

--- src/listpedeps.c
+++ src/listpedeps.c
@@ -19,14 +19,20 @@ THE SOFTWARE.
 
 #include "pestructs.h"
 #include "pedeps.h"
+#include "pedeps_version.h"
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdint.h>
 #include <string.h>
 #include <inttypes.h>
 
+#define APPLICATION_NAME "listpedeps"
+
 struct progdata_struct
 {
+  int showinfo;
+  int showimports;
+  int showexports;
   int details;
   char* lastmodule;
 };
@@ -34,7 +40,7 @@ struct progdata_struct
 int listimports (const char* modulename, const char* functionname, void* callbackdata)
 {
   struct progdata_struct* progdata = (struct progdata_struct*)callbackdata;
-  if (!progdata->details) {
+  if (progdata->details) {
     printf("%s: %s\n", modulename, functionname);
   } else {
     if (!progdata->lastmodule || strcmp(modulename, progdata->lastmodule) != 0) {
@@ -53,20 +59,48 @@ int listexports (const char* modulename, const char* functionname, uint16_t ordi
   return 0;
 }
 
+void show_help ()
+{
+  printf(
+    "Usage: " APPLICATION_NAME " [-h|-?] [-s] srcfile [...]\n"
+    "Parameters:\n"
+    "  -h -?       \tdisplay command line help\n"
+    "  -n          \tdon't show file info\n"
+    "  -i          \tlist imports\n"
+    "  -s          \tshort import list without symbols\n"
+    "  -x          \tlist exports\n"
+    "Description:\n"
+    "Lists dependencies of .exe and .dll files.\n"
+    "Version: " PEDEPS_VERSION_STRING "\n"
+    ""
+  );
+}
+
 int main (int argc, char* argv[])
 {
   int i;
   pefile_handle pehandle;
-  struct progdata_struct progdata = {1, NULL};
+  struct progdata_struct progdata = {
+    .showinfo = 1,
+    .showimports = 0,
+    .showexports = 0,
+    .details = 1,
+    .lastmodule = NULL
+  };
   int status = 0;
 
   //show version number
   printf("pedeps library version: %s\n", pedeps_get_version_string());
 
-  //determine filename
+  //check command line arguments
   if (argc <= 1) {
     fprintf(stderr, "Error: no filename given\n");
+    show_help();
     return 1;
+  }
+  if (strcmp(argv[1], "-h") == 0 || strcmp(argv[1], "-?") == 0 || strcmp(argv[1], "--help") == 0) {
+    show_help();
+    return 0;
   }
 
   //create PE object
@@ -76,8 +110,15 @@ int main (int argc, char* argv[])
   }
 
   for (i = 1; i < argc; i++) {
-    if (strcmp(argv[i], "-s") == 0) {
+    if (strcmp(argv[i], "-n") == 0) {
+      progdata.showinfo = 0;
+    } else if (strcmp(argv[i], "-i") == 0) {
+      progdata.showimports = 1;
+    } else if (strcmp(argv[i], "-s") == 0) {
+      progdata.showimports = 1;
       progdata.details = 0;
+    } else if (strcmp(argv[i], "-x") == 0) {
+      progdata.showimports = 1;
     } else {
       printf("[%s]\n", argv[i]);
       //open PE file
@@ -85,19 +126,26 @@ int main (int argc, char* argv[])
         fprintf(stderr, "Error opening PE file %s: %s\n", argv[i], pefile_status_message(status));
         return 3;
       }
-      //display information
-      printf("architecture: %s\n", pe_get_arch_name(pefile_get_machine(pehandle)));
-      printf("machine name: %s\n", pe_get_machine_name(pefile_get_machine(pehandle)));
-      printf("subsystem:    %s\n", pe_get_subsystem_name(pefile_get_subsystem(pehandle)));
-      printf("DLL:          %s\n", (pefile_is_dll(pehandle) ? "yes" : "no"));
-      printf("stripped:     %s\n", (pefile_is_stripped(pehandle) ? "yes" : "no"));
-      printf("file version: %" PRIu16 ".%" PRIu16 "\n", pefile_get_file_version_major(pehandle), pefile_get_file_version_minor(pehandle));
-      printf("minimum Windows version: %" PRIu16 ".%" PRIu16 "\n", pefile_get_min_os_major(pehandle), pefile_get_min_os_minor(pehandle));
-      //analyze file
-      printf("IMPORTS\n");
-      status = pefile_list_imports(pehandle, listimports, &progdata);
-      printf("EXPORTS\n");
-      status = pefile_list_exports(pehandle, listexports, &progdata);
+      if (progdata.showinfo) {
+        //display information
+        printf("architecture: %s\n", pe_get_arch_name(pefile_get_machine(pehandle)));
+        printf("machine name: %s\n", pe_get_machine_name(pefile_get_machine(pehandle)));
+        printf("subsystem:    %s\n", pe_get_subsystem_name(pefile_get_subsystem(pehandle)));
+        printf("DLL:          %s\n", (pefile_is_dll(pehandle) ? "yes" : "no"));
+        printf("stripped:     %s\n", (pefile_is_stripped(pehandle) ? "yes" : "no"));
+        printf("file version: %" PRIu16 ".%" PRIu16 "\n", pefile_get_file_version_major(pehandle), pefile_get_file_version_minor(pehandle));
+        printf("minimum Windows version: %" PRIu16 ".%" PRIu16 "\n", pefile_get_min_os_major(pehandle), pefile_get_min_os_minor(pehandle));
+      }
+      //list imports
+      if (progdata.showimports) {
+        printf("IMPORTS\n");
+        status = pefile_list_imports(pehandle, listimports, &progdata);
+      }
+      //list exports
+      if (progdata.showexports) {
+        printf("EXPORTS\n");
+        status = pefile_list_exports(pehandle, listexports, &progdata);
+      }
       //close PE file
       pefile_close(pehandle);
       //clean up
